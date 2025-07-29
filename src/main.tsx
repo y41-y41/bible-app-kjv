81649ede0ae64987c9038aacb405acab807964d6
y41-y41 <yazin7647@gmail.com>
2025-07-28 23:26:12 -0400

    .

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Remove this line if you don't have an index.css file
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
